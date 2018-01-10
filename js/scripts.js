436d520d62fb7e6160ace8a7267d648963aef38f
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-01-10 11:58:45 +0100

    Add working script drawing christmas tree

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+alert('Witaj w programie rysującym choinkę');
+
+do {
+	var drawTree = prompt('Podaj wysokość choinki:');
+		
+	for( var i=0; i < drawTree; i++){
+		var star = "";
+	
+		for(var j=0; j<=i ;j++){
+			star += "*";
+		
+		}
+		console.log(star);
+	}
+	zapytanie = prompt("Czy chcesz narysować kolejną choinkę? Wpisz tak lub nie.");
+	console.log(zapytanie);
+	
+}while(zapytanie.toLowerCase() == "tak"); //rób dopuki to warunek jest prawdziwy jeśli nie przerwij pentlę
+	
